c6ec8d72f039c96a82ba2e33a7e947718f381a9e
Nethajiparthinan <Nethajiparthiban@gmail.com>
2024-03-19 20:34:21 +0530

    Email Validatation completed

--- Email_validation.py
+++ Email_validation.py
@@ -1,9 +1,20 @@
 email=input("Enter your email address :")
 if len(email)>=15:
-    if email[0].isalpha():
+    if email[0].isalpha() and email[0]!=email.isdigit():
         if ("@" in email) and (email.count("@")==1):
             if (email[-4]==".") ^ (email[-3]=="."):
-                print("correct email ")
+                if email.count(".")<=1:
+                    for i in email:
+                        if i!=i.isspace():
+                            pass
+                        if i=="_" or i=="." or i=="@":
+                            print("Correct Email ")
+                            break
+                    else:
+                        print("Wrong email 6")
+
+                else:
+                    print("Wrong email 5")
             else:
                 print("Wrong email 4")
         else:
